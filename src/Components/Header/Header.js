aa052c472a822c87d5a5915d3094b109b8e145f7
brysonwaisi <brysonnyamwange@gmail.com>
2022-11-01 00:49:50 +0300

    Fix header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import BRY from '../../assets/Bry.jpg'
 import './Header.css'
+import {RiShareForwardLine} from 'react-icons/ri'
 
 function Header() {
 
   return (
-    <div className="me">
-        <img id="profile__img" src={BRY} alt="Breezy" />
+    <div className="header">
+        <img className="profile__img" src={BRY} alt="Breezy" />
+        <p>Bryson Nyamwange</p>
+        <RiShareForwardLine className="share-bar"/>
     </div>
   )
 }
