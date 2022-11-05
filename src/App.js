fe5efe3a95061d1ebc4b716243a02927ea58c80e
brysonwaisi <brysonnyamwange@gmail.com>
2022-11-05 18:20:34 +0300

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import Main from './Components/Main/Main'
+import Contact from './Components/page/Contact'
+import Error from './Components/error/Error'
 
 function App() {
-  return (
-    <div className="container-fluid mx-0 my-0">
-      <Header />
-      <Main />
-      <Footer />
-
-    </div>
-  );
+  return(
+  <>
+  <Router>
+  <Routes>
+    <Route exact path='/' element={<Main />} />
+    <Route path='/contact' element={<Contact />} /> 
+    <Route path='*' element={<Error />} />
+  </Routes>
+  <Footer />
+  </Router>
+  
+  </>
+  ) 
 }
 
 export default App;
