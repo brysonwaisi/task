4062dc5029de05c7dbce5da45e7d5abacc03c69a
brysonwaisi <brysonnyamwange@gmail.com>
2022-11-05 17:34:00 +0300

    Add Contact and error pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Route, Router, Switch } from 'react-router-dom';
+
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import Main from './Components/Main/Main'
