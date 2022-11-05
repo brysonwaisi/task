d939797bbb5d9802b30911dbad8398e990f7e35b
brysonwaisi <brysonnyamwange@gmail.com>
2022-11-05 17:21:36 +0300

    Update changes

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,17 +2,18 @@ import React from "react";
 import { data } from "./data";
 import SL from '../../assets/slack.png'
 import {AiOutlineGithub} from 'react-icons/ai'
+import uuid from "react-uuid";
 
 const Home = () =>{
     return <>
      <main className ='row mx-0 my-3'>
         <section className='col-10 my-0 mx-auto list-group'>
             {
-                data.map((item, index) => {
+                data.map((item) => {
                     const {name, url, id, title} = item
                     return<>
                         <a  className='border-0 text-center fw-normal fs-6 rounded-3 p-3 list-group-item-secondary list-group-item list-group-item-action my-2'
-                        id={id} key={index} title={title} href={url}>{name}</a>
+                        id={id} key={uuid()} title={title} href={url}>{name}</a>
                     </>
                 }
 
